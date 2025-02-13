6083c445885674d2eb5c279f694b08b45d2b4ddd
Mauricio <garciaelizarrarasluismauricio@gmail.com>
2025-02-13 08:17:48 -0700

    first commit

---
+++ introduccion/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace introduccion
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcSuma(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(tb_numb1.Text);
+            int num2 = Convert.ToInt32(tb_numb2.Text);
+            int suma = num1 + num2;
+             lb_result.Text= suma.ToString();    }
+
+        private void CalcResta(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(tb_numb1.Text);
+            int num2 = Convert.ToInt32(tb_numb2.Text);
+            int resta= num1 - num2;
+            lb_result.Text = resta.ToString();
+
+        }
+
+        private void CalcMulti(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(tb_numb1.Text);
+            int num2 = Convert.ToInt32(tb_numb2.Text);
+            int multiplicacion = num1 * num2;
+            lb_result.Text = multiplicacion.ToString();
+        }
+        private void CalcDivision(object sender, EventArgs e)
+        {
+            decimal num1 = Convert.ToInt32(tb_numb1.Text);
+            decimal num2 = Convert.ToInt32(tb_numb2.Text);
+            decimal Division = num1 / num2;
+            lb_result.Text = Division.ToString();
+        }
+        
+    }
+}
---
+++ introduccion/Form3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace introduccion
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text== "")
+            {
+                label1.Text = "Ingrese algum valor";
+            }
+            else if(textBox1.Text.Trim() =="")
+            {
+                label1.Text = "ingrese un valor";
+
+            }
+        }
+    }
+}
